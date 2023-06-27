5cb150b5af7fd31c45fd985dd6aef93fdc4cdd52
Muneeb Ahmad <91079744+Muneeb244@users.noreply.github.com>
2023-06-27 20:22:24 +0500

    completed images upload

--- src/routers/products.js
+++ src/routers/products.js
@@ -4,11 +4,33 @@ const Category = require('../models/category');
 const Product = require('../models/product');
 const mongoose = require('mongoose');
 const auth = require('../../middleware/authentication');
+const multer = require('multer');
+
+const FILE_TYPE = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid = FILE_TYPE[file.mimetype];
+        let extensionError = new Error("Invalid image type");
+
+        if (isValid) extensionError = null;
+        cb(extensionError, 'public/uploads/')
+    },
+    filename: (req, file, cb) => {
+        const extension = FILE_TYPE[file.mimetype]
+        const fileName = file.originalname.replace(' ', '-').replace(`.${extension}`, "");
+        cb(null, `${fileName}-${Date.now()}.${extension}`)
+    }
+});
+
+const upload = multer({ storage });
 
 
 router.get('/', auth, async (req, res) => {
-    console.log(req.user);
-    console.log(req.isAdmin);
     const product = await Product.find({}).populate('category');
     if (!product) return res.status(404).send('No product found.');
     res.send(product);
@@ -20,18 +42,20 @@ router.get('/:id', auth, async (req, res) => {
     res.send(product);
 });
 
-router.post('/:id', auth, async (req, res) => {
-    console.log(req.params.id);
+router.post('/:id', auth, upload.single('image'), async (req, res) => {
 
     try {
         const category = await Category.findById(req.params.id);
         if (!category) return res.status(400).send('Invalid Category');
 
+        const image = req.file?.filename;
+        if (!image) return res.status(400).send('No image in the request');
+        const imagePath = `${req.protocol}://${req.get('host')}/public/uploads/${image}`;
+
         const {
             name,
             description,
             richDescription,
-            image,
             brand,
             price,
             countInStock,
@@ -39,11 +63,11 @@ router.post('/:id', auth, async (req, res) => {
             numReviews,
             isFeatured
         } = req.body;
-        const product = new Product({
+        let product = new Product({
             name,
             description,
             richDescription,
-            image,
+            image: imagePath,
             brand,
             price,
             category,
@@ -61,12 +85,23 @@ router.post('/:id', auth, async (req, res) => {
 
 });
 
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', auth, upload.single('image'), async (req, res) => {
 
     if (mongoose.isValidObjectId(req.params.id)) return res.status(400).send('Invalid Product Id');
 
     const category = await Category.findById(req.body.category);
     if (!category) return res.status(400).send('Invalid Category');
+
+    const oldProduct = await Product.findById(req.params.id);
+    if (!oldProduct) return res.status(400).send('Invalid Product');
+
+
+    let imagePath;
+    if (req.file) {
+        const image = req.file?.filename;
+        imagePath = `${req.protocol}://${req.get('host')}/public/uploads/${image}`;
+    }
+    else imagePath = oldProduct.image;
 
     const {
         name,
@@ -84,7 +119,7 @@ router.put('/:id', auth, async (req, res) => {
         name,
         description,
         richDescription,
-        image,
+        image: imagePath,
         brand,
         price,
         countInStock,
@@ -108,18 +143,33 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+
+router.put('/gallery-images/:id', auth, upload.array('images', 10), async (req, res) => {
+    // if (mongoose.isValidObjectId(req.params.id)) return res.status(400).send('Invalid Product Id');
+
+    if(!req.files) return res.status(400).send('No images in the request');
+
+    const product = await Product.findByIdAndUpdate(req.params.id, {
+        images: req.files.map(file => `${req.protocol}://${req.get('host')}/public/uploads/${file.filename}`)
+    }, { new: true });
+
+    if (!product) return res.status(404).send('Product cannot be updated .');
+    res.send(product);
+
+});
+
 // router.get('/count', async (req, res) => {
 //     res.send('Hello');
 
-    // try {
-    //     const productCount = await Product.countDocuments((count) => console.log(count));
-    //     console.log(productCount);
-    //     if (!productCount) return res.status(500).send('No product found.');
-    //     res.send(productCount);
-    // }
-    // catch (error) {
-    //     res.status(500).send('Error getting product count.');
-    // }
+// try {
+//     const productCount = await Product.countDocuments((count) => console.log(count));
+//     console.log(productCount);
+//     if (!productCount) return res.status(500).send('No product found.');
+//     res.send(productCount);
+// }
+// catch (error) {
+//     res.status(500).send('Error getting product count.');
+// }
 // });
 
 
